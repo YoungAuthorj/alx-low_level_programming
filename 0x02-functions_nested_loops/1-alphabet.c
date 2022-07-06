65c8bb70d0be673a6b2201852b587b91828c1e9d
YoungAuthorj <youngauthorjozef@yahoo.com>
2022-07-05 18:07:40 -0700

    C-func

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,13 @@
 
 void print_alphabet(void)
 {
-char c = 'a';
-while (c <= 'z')
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
-_putchar(c);
-c++; 
+_putchar(alphabet);
 }
+
 _putchar('\n');
 
 }
